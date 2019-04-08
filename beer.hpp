07f2d4597f7e875d56fbb58a24183e907346d908
luckym1 <45721584+luckym1@users.noreply.github.com>
2019-04-08 14:29:20 -0600

    Update beer.hpp

--- beer.hpp
+++ beer.hpp
@@ -24,19 +24,45 @@ struct recipe{
 
 class Hash{
 private:
+    //used for memory debug
     int alloc;
     int dealloc;
+    
+    //max table size
     int TableSize;
+    
+    //array of recipe structs used for hash table
     recipe * * table;
+    
+    //hashing function for string type into integer
     int hash(string name);
+    
+    //current number of recipies in hash table
     int currSize;
 public:
+    //constructor with default table size of 1000
     Hash(int maxSize = 1000);
+    //destructor
     ~Hash(void);
+    
+    //Input: fully initialized recipe struct
+    //Output: memory allocated for struct and pointer to struct stored in proper
+    //        place in hash table
     void insert(recipe beerRecipe);
+    
+    //Overloaded function 
+    //Input: string or double and a string of the parameter type to search
+    //Output: vector of recipe's that contain the given parameter
     recipe * search(string key, string parameter);
     recipe * search(double key, string parameter);
+    
+    //Input: string to name of recipe
+    //Output: Recipe struct removed from hash table, memory deallocated 
     void del(string name);
+    
+    //Input: must have a initialized Hash object
+    //Output: Printed to screen the number of each "bucket" in the hash array
+    //        and the recipies that are currently inside each bucket
     void print(void);
 };
 
